617dc3b77918b91d30ce891d2a978ca226e7444a
Ashrulez <kimaniashnice@gmail.com>
2022-10-06 09:21:59 -0700

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,27 +16,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s;
 
 	if (s1 == NULL)
-	{
 		i = 0;
-	}
 	else
 	{
 		for (i = 0 ; s1[i] ; i++)
-		{
 			;
-		}
 	}
 
 	if (s2 == NULL)
-	{
 		j = 0;
-	}
 	else
 	{
 		for (j = 0 ; s2[j] ; j++)
-		{
 			;
-		}
 	}
 
 	if (j > n)
@@ -46,9 +38,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	s = malloc(sizeof(char) * (i + j + 1));
 
 	if (s == NULL)
-	{
 		return (NULL);
-	}
 
 	for (k = 0 ; k < i ; k++)
 	{
